348243891750c7247230a0f1c018dd0cc6f2a98f
prateekYadav07 <yadavprateek39@gmail.com>
2023-05-15 08:57:56 +0530

    configuration power of spring boot

--- learn-spring-boot/src/main/java/com/in28minutes/springboot/learnspringboot/CourseController.java
+++ learn-spring-boot/src/main/java/com/in28minutes/springboot/learnspringboot/CourseController.java
@@ -3,6 +3,7 @@ package com.in28minutes.springboot.learnspringboot;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,11 @@ public class CourseController {
 	@RequestMapping("/courses")
 	public List<Course> retrieveAllCourses() {
 		return Arrays.asList(new Course(1, "Learn Springboot", "in28minutes"),
-				new Course(2, "Learn DSA", "in28minutes"));
+				new Course(2, "Learn DSA", "in28minutes"),
+				new Course(3, "Learn Azure", "in28minutes"),
+				new Course(3, "Learn Python", "in28minutes")
+				
+				);
 	}
 	
 	@RequestMapping("/")
