f31b00706f2d2dc062faf7fbac5abdb4626353ae
prateekYadav07 <yadavprateek39@gmail.com>
2023-05-21 14:41:53 +0530

    Request parameters

--- myFirstSpringApp/src/main/java/com/in28minutes/springboot/myFirstSpringApp/login/LoginController.java
+++ myFirstSpringApp/src/main/java/com/in28minutes/springboot/myFirstSpringApp/login/LoginController.java
@@ -1,14 +1,24 @@
 package com.in28minutes.springboot.myFirstSpringApp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class LoginController{
+public class LoginController {
 	
+	// logger level is defined in application file, like info, warn, debug
+//	private Logger logger = LoggerFactory.getLogger(getClass());
+	// http://localhost:8080/login?name=Steve
 	@RequestMapping("login")
-	public String sayHelloJsp() {
+	public String sayHelloJsp(@RequestParam String name, String age, ModelMap model) {
+		model.put("name", name);
+		model.put("age", age);
+//		logger.debug("Request params are name: {} age: {}",name, age);
 		return "login";
 	}
-	
+
 }
