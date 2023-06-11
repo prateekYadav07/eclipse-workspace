02ac8b88e1e0a3b567cf89e278864e881221129d
prateekYadav07 <yadavprateek39@gmail.com>
2023-06-11 17:20:21 +0530

    filtering, version control and HATEOS

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/users/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/users/User.java
@@ -2,6 +2,8 @@ package com.in28minutes.rest.webservices.restfulwebservices.helloworld.users;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -9,8 +11,10 @@ public class User {
 
 	private long id;
 	@Size(min = 2, message = "Name should be atleast 2 characters long")
+	@JsonProperty("user_name")
 	private String name;
 	@Past(message = "Birth date should be in the past")
+	@JsonProperty("birth_date")
 	private LocalDate birthDate;
 
 	public User() {
--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
@@ -6,6 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VersioningPersonController {
 
+	/**
+	 * @implNote different type of versioning
+	 * @types : 1. differnet URL 2. Request parameters 3. Custom request headers 4.
+	 *        Media type versioning
+	 * @return
+	 */
+
 	@GetMapping("v1/person")
 	public PersonV1 getv1Person() {
 		return new PersonV1("Bob Noyce");
@@ -23,6 +30,26 @@ public class VersioningPersonController {
 
 	@GetMapping(path = "person", params = "version=2")
 	public PersonV2 getv2PersonByReqParam() {
+		return new PersonV2(new Name("Bob", "Noyce"));
+	}
+
+	@GetMapping(path = "person/header", headers = "X-API-VERSION=1")
+	public PersonV1 getv1PersonByReqHeader() {
+		return new PersonV1("Bob Noyce");
+	}
+
+	@GetMapping(path = "person/header", headers = "X-API-VERSION=2")
+	public PersonV2 getv2PersonByReqHeader() {
+		return new PersonV2(new Name("Bob", "Noyce"));
+	}
+
+	@GetMapping(path = "person/header", produces = "application/vnd.company.app-v1+json")
+	public PersonV1 getv1PersonByAcceptHeader() {
+		return new PersonV1("Bob Noyce");
+	}
+
+	@GetMapping(path = "person/header", produces = "application/vnd.company.app-v2+json")
+	public PersonV2 getv2PersonByAcceptHeader() {
 		return new PersonV2(new Name("Bob", "Noyce"));
 	}
 }
