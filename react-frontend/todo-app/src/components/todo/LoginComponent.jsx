13fb28f00ceefa86c85413d5914fbee703395aca
prateekYadav07 <yadavprateek39@gmail.com>
2023-06-28 09:35:02 +0530

    hardcoded login

--- react-frontend/todo-app/src/components/todo/LoginComponent.jsx
+++ react-frontend/todo-app/src/components/todo/LoginComponent.jsx
@@ -4,6 +4,9 @@ import "../../../src/Login.css";
 export default function LoginComponent() {
     const [username, setUserName] = useState("in28minutes")
     const [password, setPassword] = useState("dummy")
+    const [showSuccessMsg, setShowSuccessMsg] = useState(false)
+    const [showFailureMsg, setShowFailureMsg] = useState(false)
+
 
     function usernameHandler(event) {
         setUserName(event.target.value);
@@ -13,19 +16,34 @@ export default function LoginComponent() {
         setPassword(event.target.value);
     }
 
+    function handleLogin() {
+        if (username === 'in28minutes' && password === 'dummy') {
+            console.log("success");
+            setShowSuccessMsg(true)
+            setShowFailureMsg(false)
+        }
+        else {
+            console.log('failure');
+            setShowSuccessMsg(false)
+            setShowFailureMsg(true)
+        }
+    }
+
     return (
         <div className="login">
+            <SuccessMsgComponent showSuccessMsg={showSuccessMsg} />
+            <FailureMsgComponent showFailureMsg={showFailureMsg} />
             <div className="wrapper fadeInDown">
                 <div id="formContent">
                     <div className="fadeIn first">
-                        <img src="" id="icon" alt="Todo Management App" />
+                        {/* <img src="" id="icon" alt="Todo Management App" /> */}
                     </div>
 
-                    <form>
-                        <input type="text" id="login" className="fadeIn second" name="login" placeholder="username" value={username} onChange={usernameHandler} />
-                        <input type="password" id="password" className="fadeIn third" name="login" placeholder="password" value={password} onChange={passwordHandler} />
-                        <input type="submit" className="fadeIn fourth" value="Log In" />
-                    </form>
+                    {/* <form> */}
+                    <input type="text" id="login" className="fadeIn second" name="login" placeholder="username" value={username} onChange={usernameHandler} />
+                    <input type="password" id="password" className="fadeIn third" name="login" placeholder="password" value={password} onChange={passwordHandler} />
+                    <input type="submit" className="fadeIn fourth" value="Log In" onClick={handleLogin} />
+                    {/* </form> */}
 
                     <div id="formFooter">
                         <a className="underlineHover" href="#">Forgot Password?</a>
@@ -34,4 +52,14 @@ export default function LoginComponent() {
             </div>
         </div>
     )
+}
+
+function SuccessMsgComponent({ showSuccessMsg }) {
+    if (showSuccessMsg)
+        return <div className="alert alert-primary" role="alert">Authentication successful</div>
+}
+
+function FailureMsgComponent({ showFailureMsg }) {
+    if (showFailureMsg)
+        return <div className="alert alert-danger" role="alert">Authentication Failure, incorrect username and password</div>
 }
