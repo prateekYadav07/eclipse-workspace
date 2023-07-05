d874b29f8375566b6246091a0f71cd6605f10cb8
prateekYadav07 <yadavprateek39@gmail.com>
2023-07-05 08:30:17 +0530

    react context

--- react-frontend/todo-app/src/components/todo/WelcomeComponent.jsx
+++ react-frontend/todo-app/src/components/todo/WelcomeComponent.jsx
@@ -1,6 +1,4 @@
 import { useParams, Link } from "react-router-dom"
-import HeaderComponent from "./HeaderComponent"
-import FooterComponent from "./FooterComponent"
 
 export default function WelcomeComponent() {
     const { username } = useParams()
@@ -8,12 +6,10 @@ export default function WelcomeComponent() {
     return (
         <>
             <div className="welcome">
-                <HeaderComponent username={username}/>
                 <h1>Welcome to Todos, {username}</h1>
                 <div>
                     <Link to="/list-todos">Manage your todos</Link>
                 </div>
-                <FooterComponent />
             </div>
         </>
     )
